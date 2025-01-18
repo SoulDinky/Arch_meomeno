3d1a175c357718d82e4007b25da479091e711df3
SoulDinky <162184452+SoulDinky@users.noreply.github.com>
2025-01-18 17:19:02 +0200

    Add files
    AutoInstalls

---
+++ meomeno/meomeno_app.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+sudo pacman -S \ 
+    thunderbird \
+	kicad \
+    blender \
+	gnumeric \
+    featherpad \
+	alsa-plugins \
+	alsa-utils \
+	breeze \
+	celluloid \
+	clipgrab \
+	ffmpeg \
+	firefox \
+	firefox-ublock-origin \
+    gnome-calculator \
+	gnome-disk-utility \
+	gnome-screenshot \
+	gufw \
+	htop \
+	lightdm \
+	lightdm-gtk-greeter \
+	lightdm-gtk-greeter-settings \
+	man-db \
+	man-pages \
+    network-manager-applet \
+    neofetch \
+	papirus-icon-theme \
+	pavucontrol \
+	pcmanfm \
+	pulseaudio \
+	pulseaudio-alsa \
+	terminus-font \
+	volumeicon \
+	xed \
+	xfce4-power-manager \
+	xorg-server \
+	xorg-xinit \
+	xorg-xrandr \
+	xterm \
+	xvidcore \
+	zsh \
+	zsh-syntax-highlighting
