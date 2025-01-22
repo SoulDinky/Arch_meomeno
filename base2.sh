1b1718f0d8fb0cd4fac9addf18ddf407e85ffc9f
SoulDinky <162184452+SoulDinky@users.noreply.github.com>
2025-01-22 12:06:42 +0200

    Update base2.sh
    version 2

--- base2.sh
+++ base2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils hplip alsa-utils pulseaudio pulseaudio-alsa pavucontrol bash-completion rsync reflector acpi acpi_call virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S network-manager-applet wpa_supplicant alsa-utils pulseaudio pulseaudio-alsa pavucontrol bash-completion rsync reflector sof-firmware terminus-font mpv
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB 
 grub-mkconfig -o /boot/grub/grub.cfg
